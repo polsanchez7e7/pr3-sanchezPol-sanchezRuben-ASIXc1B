87cea0d8e9e9fa80630d9dc9c7c49fbd988ad0d4
pol.sanchez.7e7 <pol.sanchez.7e7@itb.cat>
2023-11-08 12:55:47 +0100

    .

--- pr3/customerLoyaltyCard.py
+++ pr3/customerLoyaltyCard.py
@@ -6,9 +6,15 @@ M03 UF1 A3 PR3
 08/11/2023
 
 '''
+try:
+    targetaclient = input("Tens targeta client? (S/N) ")
+    importfactura = float(input("introdueix l'import amb l'IVA inclós "))
 
-targetaclient = input("Tens targeta client de client? ")
-importfactura = float(input("introdueix l'import amb l'IVA inclós "))
+    if targetaclient == "S" and importfactura >= 100:
+        importdescompte = (importfactura * 0.79) * 1.21
+        print(f"L'import a pagar és, {importdescompte:.2f}")
+    elif targetaclient == "N":
+        print("No tens descompte de client")
+except ValueError:
+    print("ERROR: Introdueix el numero només")
 
-if importfactura >= 100:
-    importfactura * 0.79
