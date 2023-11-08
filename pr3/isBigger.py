4840cc2a0125463afa22a8dbd87a2ca29095f445
pol.sanchez.7e7 <pol.sanchez.7e7@itb.cat>
2023-11-08 11:59:48 +0100

    .

--- pr3/isBigger.py
+++ pr3/isBigger.py
@@ -6,5 +6,15 @@ M03 UF1 A3 PR3
 08/11/2023
 
 '''
+
 primernumero = int(input("Introdueix el primer numero "))
 segonnumero = int(input("Introdueix el segon numero "))
+intercanvi = (int())
+
+if primernumero > segonnumero:
+    intercanvi = primernumero
+    primernumero = segonnumero
+    segonnumero = intercanvi
+    print ("El primer numero és", primernumero, "El segon numero és", segonnumero)
+else:
+    print("El primer numero no és més gran que el segon ")
