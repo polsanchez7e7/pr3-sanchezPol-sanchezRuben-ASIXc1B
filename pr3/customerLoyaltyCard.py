255460f1f6abcb299eaa351042da33224d1dc297
pol.sanchez.7e7 <pol.sanchez.7e7@itb.cat>
2023-11-08 12:42:07 +0100

    .

---
+++ pr3/customerLoyaltyCard.py
@@ -0,0 +1,14 @@
+'''
+Pol Sánchez
+Trishan Mizhquiri
+Rubén Sánchez
+M03 UF1 A3 PR3
+08/11/2023
+
+'''
+
+targetaclient = input("Tens targeta client de client? ")
+importfactura = float(input("introdueix l'import amb l'IVA inclós "))
+
+if importfactura >= 100:
+    importfactura * 0.79
