a57baea3f9a72b036d15e26f20880f890e91cc00
pol.sanchez.7e7 <pol.sanchez.7e7@itb.cat>
2023-11-08 12:22:13 +0100

    .

---
+++ pr3/fastAndFurios.py
@@ -0,0 +1,26 @@
+'''
+Pol Sánchez
+Trishan Mizhquiri
+Rubén Sánchez
+M03 UF1 A3 PR3
+08/11/2023
+
+'''
+
+try:
+    velocitatinicial = float(input("Introdueix la velocitat inicial "))
+    acceleracio = float(input("Introdueix l'acceleració "))
+    temps = float(input("Introdueix el temps "))
+
+    velocitatinstantania = velocitatinicial + acceleracio * temps
+    velocitatmitjana = (velocitatinicial + velocitatinstantania )/2
+
+    if velocitatinstantania <= 0:
+        print("La velocitat mitjana no és calculable ")
+
+    else:
+        print("La velocitat instantania és", velocitatinstantania, "La velocitat mitjana és", velocitatmitjana)
+except ValueError:
+    print("ERROR: Introdueix els numeros només")
+
+
