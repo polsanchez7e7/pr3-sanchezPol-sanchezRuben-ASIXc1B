1baef2ff77540fbe1e8a408a859bf68ff028ab53
pol.sanchez.7e7 <pol.sanchez.7e7@itb.cat>
2023-11-08 12:12:35 +0100

    .

--- pr3/areGrowingNumbers.py
+++ pr3/areGrowingNumbers.py
@@ -6,12 +6,15 @@ M03 UF1 A3 PR3
 08/11/2023
 
 '''
+try:
+    primernumero = int(input("Introdueix el primer numero "))
+    segonnumero = int(input("introdueix el segon numero "))
+    tercernumero = int(input("Introdueix el tercer numero "))
 
-primernumero = int(input("Introdueix el primer numero "))
-segonnumero = int(input("introdueix el segon numero "))
-tercernumero = int(input("Introdueix el tercer numero "))
+    if primernumero < segonnumero < tercernumero:
+        print("L'ordre dels numeros és creixent ")
 
-if primernumero < segonnumero < tercernumero:
-    print("L'ordre dels numeros és creixent ")
-
-else:
+    else:
+        print("L'ordre dels numeros no és creixent")
+except ValueError:
+    print("ERROR: Introdueix numeros enters")
