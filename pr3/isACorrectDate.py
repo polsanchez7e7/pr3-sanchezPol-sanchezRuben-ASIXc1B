8cf801e688f3fcb45adc499880fc191f8e4cda91
pol.sanchez.7e7 <pol.sanchez.7e7@itb.cat>
2023-11-09 12:09:56 +0100

    .

--- pr3/isACorrectDate.py
+++ pr3/isACorrectDate.py
@@ -9,21 +9,25 @@ M03 UF1 A3 PR3
 
 data = input("Introdueix la data en format DD/MM/AAAA")
 dia, mes, any = data.split("/")
+bisiesto = False
 
 if any % 4 == 0 and (any %100 != 0 or any % 400 == 0):
-        return True
-    else:
-        return False
+     bisiesto = True
 
 
 
 
-if False:
+
+
+if not bisiesto: #bisiesto == False:
     if mes == (01 or 03 or 05 or 07 or 08 or 10 or 12):
         match dia:
-            case 1=> dia <=31: print("Correcte")
+            case (1=> dia <=31): print("Correcte")
             case _: print("No és un dia correcte")
-    elif mes == 04 or 06 or 09 or 11
+    elif mes == (04 or 06 or 09 or 11):
+        match dia:
+            case (1=> dia <=30): print("Correcte")
+    else:
 
 
 
